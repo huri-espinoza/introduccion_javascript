fc7ae81d5bb403f65adb32ce634f9e648f93b18c
Huri Espinoza <huri.espinoza.i@gmail.com>
2025-10-04 23:45:49 -0300

    se agrego logica para sumar y restar cantidad y obtener valor total

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-precio = 400000
+const precio = 400000
 
-precioSpan = document.querySelector(".precio-inicial");
+let precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio
+
+let total = document.querySelector(".valor-total");
+total.innerHTML= 0
+
+
+function sumar() {
+    let cantidad = document.querySelector(`.cantidad`);
+    let aumentar = Number (cantidad.innerHTML);
+    aumentar = aumentar + 1;
+    cantidad.innerHTML= aumentar;
+
+    let total = document.querySelector(`.valor-total`);
+    total.innerHTML = aumentar * precio;
+}
+
+
+function restar() {
+    let cantidad = document.querySelector(`.cantidad`);
+    let disminuir = Number (cantidad.innerHTML);
+    disminuir = disminuir - 1;
+    cantidad.innerHTML= disminuir;
+
+    let total = document.querySelector(`.valor-total`);
+    total.innerHTML= disminuir * precio;
+}
